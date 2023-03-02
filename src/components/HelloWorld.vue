3549fd150d024338bb57181c988e414c23a56e99
Alexis TAN <alexistan9712@gmail.com>
2023-03-02 15:54:36 +0100

    ✨ Add Vue3 Starter

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+	<div class="greetings">
+		<h1 class="green">{{ msg }}</h1>
+		<h3>
+		You’ve successfully created a project with
+		<a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+		<a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+		</h3>
+	</div>
+</template>
+<style lang="postcss" scoped>
+h1 {
+	font-weight: 500;
+	font-size: 2.6rem;
+	top: -10px;
+
+	.greetings {
+		text-align: center;
+
+		@media screen and (min-width: 1024px) {
+			text-align: left;
+
+		}
+	}
+}
+
+h3 {
+	font-size: 1.2rem;
+
+	.greetings {
+		text-align: center;
+
+		@media screen and (min-width: 1024px) {
+			text-align: left;
+
+		}
+	}
+}
+
+</style>
